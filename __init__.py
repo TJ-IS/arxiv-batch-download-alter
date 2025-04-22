9b48c09b9e1dd9ee196573d2adc05d3376a892a0
HXHY <jizhilieshou@gmail.com>
2025-04-22 17:26:35 +0800

    change max_workers defaults

--- __init__.py
+++ __init__.py
@@ -11,13 +11,11 @@ if __name__ == '__main__':
     """
     arxiv网站关键词论文下载脚本
     args:
-        papers_info_core:
-            keywords: 关键词【可修改】
-            page_size: 爬取速率[25/50/100/200]【可修改】
-        papers_file_core:
-            path_of_csv: 总论文信息csv文件路径【默认不需要修改】
-            proxies_port: 使用代理端口，不填则使用临时本地端口【网速慢可挂VPN后修改为对应端口】
-            max_workers: 线程池中的线程数【与本地网速有关，默认为3】
+        keywords: 关键词【可修改】
+        page_size: 爬取速率[25/50/100/200]【可修改】
+        path_of_csv: 总论文信息csv文件路径【默认不需要修改】
+        proxies_port: 使用代理端口，不填则使用临时本地端口【网速慢可挂VPN后修改为对应端口】
+        max_workers: 线程池中的线程数【与本地网速有关，默认为3】
     """
     papers_info_core(keywords="text spotter", page_size=200, proxies_port=10808)
-    papers_file_core(path_of_csv="paper_result.csv", proxies_port=10808, max_workers=10)
+    papers_file_core(path_of_csv="paper_result.csv", proxies_port=10808, max_workers=3)
